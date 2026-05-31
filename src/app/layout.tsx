b7bb45992aeffc570360d07251e0adb17cafd32b
Maxim <m.leppyanenn@gmail.com>
2026-05-31 15:40:13 +0200

    добавить Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Inter } from 'next/font/google'
 import { SessionProvider } from '@/components/layout/SessionProvider'
 import { CookieProvider } from '@/components/layout/CookieProvider'
@@ -34,7 +35,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru" className={inter.variable}>
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MZQGK36D');`,
+          }}
+        />
+      </head>
       <body className="min-h-screen bg-white antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MZQGK36D"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <SessionProvider>
           {/* CookieProvider: показывает баннер согласия и передаёт cookiesAllowed через Context */}
           <CookieProvider>
